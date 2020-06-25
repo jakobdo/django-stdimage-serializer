b0b9bc415f72ce70ebedc3cb0050ba7ace32c977
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-06-25 10:35:59 +0200

    Version changed to work with Django Rest Framework 3.X
    Also changed to work in Python 3

--- stdimage_serializer/fields.py
+++ stdimage_serializer/fields.py
@@ -6,9 +6,6 @@ class StdImageField(serializers.ImageField):
     Get all the variations of the StdImageField
     """
 
-    def to_native(self, obj):
-        return self.get_variations_urls(obj)
-
     def to_representation(self, obj):
         return self.get_variations_urls(obj)
 
@@ -23,23 +20,32 @@ class StdImageField(serializers.ImageField):
         # Get the field of the object
         field = obj.field
 
-        # A lot of ifs going araound, first check if it has the field variations
+        # A lot of ifs going araound,
+        # first check if it has the field variations
         if hasattr(field, 'variations'):
             # Get the variations
             variations = field.variations
             # Go through the variations dict
-            for key in variations.keys():
-                # Just to be sure if the stdimage object has it stored in the obj
+            for key, attr in variations.items():
+                # Just to be sure if the stdimage
+                # object has it stored in the obj
                 if hasattr(obj, key):
                     # get the by stdimage properties
-                    field_obj = getattr(obj, key, None)
-                    if field_obj and hasattr(field_obj, 'url'):
-                        # store it, with the name of the variation type into our return object
-                        return_object[key] = super(StdImageField, self).to_representation(field_obj)
+                    fieldObj = getattr(obj, key, None)
+                    if fieldObj:
+                        # Check if there is an url
+                        url = getattr(fieldObj, 'url', None)
+                    if url:
+                        # store it, with the name of the
+                        # variation type into our return object
+                        return_object[key] = url
 
         # Also include the original (if possible)
         if hasattr(obj, 'url'):
-            return_object['original'] = super(StdImageField, self).to_representation(obj)
+            return_object['original'] = obj.url
 
         return return_object
 
+    def to_internal_value(self, data):
+        """ Just go with the flow """
+        return super(serializers.ImageField, self).to_internal_value(data)
